62789aa47c2ff5ead2475ec7d692d32abac063e2
ewong18 <37660460+ewong18@users.noreply.github.com>
2018-04-10 22:31:58 -0500

    Add files via upload

---
+++ ClockZoomForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EscapeFromTheOffice
+{
+    public partial class ClockZoomForm : Form
+    {
+        public ClockZoomForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PicBoxDownArrow_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
